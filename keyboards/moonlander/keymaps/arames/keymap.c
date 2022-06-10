946cec02a807e8f46a85fb0f84c6761291524f43
Alexandre Rames <arames@tesla.com>
2022-06-10 11:29:40 -0700

    Add a dedicated key for underscore.

--- keyboards/moonlander/keymaps/arames/keymap.c
+++ keyboards/moonlander/keymaps/arames/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----+-----+-----+-----+-----+-----|-----|     |------|-----+-----+-----+-----+-----+-----|
    * |  '  |  A  |  S  |  D  |  F  |  G  | Del |     |Bspace|  H  |  J  |  K  |  L  |  ;  |  -  |
    * |-----+-----+-----+-----+-----+-----|-----'     `------|-----+-----+-----+-----+-----+-----|
-   * |     |  Z  |  X  |  C  |  V  |  B  |                  |  N  |  M  |  ,  |  .  |  /  |     |
+   * |     |  Z  |  X  |  C  |  V  |  B  |                  |  N  |  M  |  ,  |  .  |  /  |  _  |
    * `-----------------------------------'                  `-----------------------------------|
    * | DF1 |     |     |     | Alt |                              | Alt |     |     |     |     |
    * `-----------------------------'                              `-----------------------------'
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC , KC_1 , KC_2 , KC_3 , KC_4   , KC_5  , KC_NO , /*|*/ KC_NO  , KC_6  , KC_7   , KC_8   , KC_9  , KC_0   , KC_EQL ,
       KC_NO  , KC_Q , KC_W , KC_E , KC_R   , KC_T  , KC_TAB, /*|*/ KC_TAB , KC_Y  , KC_U   , KC_I   , KC_O  , KC_P   , KC_NO  ,
       KC_QUOT, KC_A , KC_S , KC_D , KC_F   , KC_G  , KC_DEL, /*|*/ KC_BSPC, KC_H  , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_MINS,
-      KC_NO  , KC_Z , KC_X , KC_C , KC_V   , KC_B  ,         /*|*/          KC_N  , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_NO  ,
+      KC_NO  , KC_Z , KC_X , KC_C , KC_V   , KC_B  ,         /*|*/          KC_N  , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_UNDS,
       DF(1)  , KC_NO, KC_NO, KC_NO, KC_LALT,         KC_NO , /*|*/ KC_NO  ,         KC_RALT, KC_NO  , KC_NO , KC_NO  , KC_NO  ,
                                     KC_SPC , KC_ENT, KC_NO , /*|*/ KC_NO  , KC_ENT, KC_SPC
   ),
